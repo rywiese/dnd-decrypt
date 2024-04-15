3c9260ba0d78cd1986d5d924990dd210ffea7b66
Ry Wiese <ry.wiese@gmail.com>
2024-04-15 14:59:50 -0500

    Implement ciphers as composition of functions rather than hierarchy of explicitly defined objects/classes

--- src/main/kotlin/Cipher.kt
+++ src/main/kotlin/Cipher.kt
@@ -2,188 +2,217 @@ interface Cipher {
 
     val name: String
 
-    fun encipher(plainText: String): String {
-        val plainWords: List<String> = plainText.split(' ')
-        val cipherWords: List<String> = encipher(plainWords)
-        return cipherWords.joinToString(separator = " ")
-    }
+    fun encipher(plainText: String): String
 
-    fun decipher(cipherText: String): String {
-        val cipherWords: List<String> = cipherText.split(' ')
-        val plainWords: List<String> = decipher(cipherWords)
-        return plainWords.joinToString(separator = " ")
-    }
+    fun decipher(cipherText: String): String
 
-    fun encipher(plainWords: List<String>): List<String>
+    companion object {
 
-    fun decipher(cipherWords: List<String>): List<String>
-
-    object Noop : Cipher {
-
-        override val name: String = "NOOP"
-
-        override fun encipher(plainWords: List<String>): List<String> = plainWords
-
-        override fun decipher(cipherWords: List<String>): List<String> = cipherWords
-
-    }
-
-    interface Substitution : Cipher {
-
-        override fun encipher(plainWords: List<String>): List<String> =
-            applySubstitutions(plainWords, ::encipherSubstitution)
-
-        override fun decipher(cipherWords: List<String>): List<String> =
-            applySubstitutions(cipherWords, ::decipherSubstitution)
-
-        fun encipherSubstitution(plainChar: Char, index: Int): Char
-
-        fun decipherSubstitution(cipherChar: Char, index: Int): Char
-
-        private fun applySubstitutions(
-            words: List<String>,
-            substitute: (Char, Int) -> Char
-        ): List<String> =
-            words.mapIndexed { wordIndex: Int, word: String ->
-                val numPreviousChars: Int = words.take(wordIndex)
-                    .fold(0) { totalChars: Int, previousWord: String ->
-                        totalChars + previousWord.length
-                    }
-                word
-                    .mapIndexed { charIndex: Int, char: Char ->
-                        val index: Int = numPreviousChars + charIndex
-                        substitute(char, index)
-                    }
-                    .joinToString(separator = "")
+        val noop: Cipher = wordedTextCipher(
+            name = "NOOP",
+            encipher = { plainWords: List<String> ->
+                plainWords
+            },
+            decipher = { cipherWords: List<String> ->
+                cipherWords
             }
+        )
 
-    }
+        val caesar = shift(-3)
 
-    object Caesar : Substitution by Shift(-3) {
+        // Note that this could also be a PermutedAlphabet
+        fun shift(shift: Int): Cipher = affine(factor = 1, shift = shift)
 
-        override val name: String = "Caesar"
+        fun affine(factor: Int, shift: Int): Cipher = substitution(
+            name = "Affine with formula `$factor * x + $shift mod ${plaintextAlphabet.length()}`",
+            encipherChar = { plainChar: Char, _: Int ->
+                plainChar.transform(factor, shift)
+            },
+            decipherChar = { cipherChar: Char, _: Int ->
+                val inverseFactor: Int = multiplicativeInverse(factor)
+                    ?: throw IllegalArgumentException("$factor must be coprime with ${plaintextAlphabet.length()}")
+                cipherChar.invertTransformation(inverseFactor, shift)
+            }
+        )
 
-    }
+        fun vigenere(keyword: String): Cipher = substitution(
+            name = "Vigenère with keyword $keyword",
+            encipherChar = { plainChar: Char, index: Int ->
+                plainChar.shiftBy(shiftChar = keyword[index.mod(keyword.length)])
+            },
+            decipherChar = { cipherChar: Char, index: Int ->
+                cipherChar.shiftBackBy(shiftChar = keyword[index.mod(keyword.length)])
+            }
+        )
 
-    // Note that this could also be a PermutedAlphabet
-    class Shift(val shift: Int) : Substitution by Affine(
-        factor = 1,
-        shift = shift
-    ) {
+        fun simpleSubstitution(keyword: String): Cipher = permutedAlphabet(
+            name = "Simple Substitution with keyword $keyword",
+            permutedAlphabet = plaintextAlphabet.permuteWithPrefix(keyword.removeDuplicates())
+        )
 
-        override val name: String = "Shift with shift $shift"
+        val atbash: Cipher = permutedAlphabet(
+            name = "Atbash",
+            permutedAlphabet = plaintextAlphabet.reversed()
+        )
 
-    }
-
-    class Affine(val factor: Int, val shift: Int) : Substitution {
-
-        override val name: String = "Affine with formula `$factor * x + $shift mod ${plaintextAlphabet.length()}`"
-
-        val inverseFactor: Int = multiplicativeInverse(factor)
-            ?: throw IllegalArgumentException("$factor must be coprime with ${plaintextAlphabet.length()}")
-
-        override fun encipherSubstitution(plainChar: Char, index: Int): Char =
-            plainChar.transform(factor, shift)
-
-        override fun decipherSubstitution(cipherChar: Char, index: Int): Char =
-            cipherChar.invertTransformation(inverseFactor, shift)
-
-    }
-
-    class Vigenere(val keyword: String) : Substitution {
-
-        override val name: String = "Vigenère with keyword $keyword"
-
-        override fun encipherSubstitution(plainChar: Char, index: Int): Char =
-            plainChar.shiftBy(shiftChar = keyword[index.mod(keyword.length)])
-
-        override fun decipherSubstitution(cipherChar: Char, index: Int): Char =
-            cipherChar.shiftBackBy(shiftChar = keyword[index.mod(keyword.length)])
-
-    }
-
-    interface PermutedAlphabet : Substitution {
-
-        val permutedAlphabet: Alphabet
-
-        override fun encipherSubstitution(plainChar: Char, index: Int): Char =
-            permutedAlphabet[plaintextAlphabet.indexOf(plainChar)]
-
-        override fun decipherSubstitution(cipherChar: Char, index: Int): Char =
-            plaintextAlphabet[permutedAlphabet.indexOf(cipherChar)]
-
-    }
-
-    class SimpleSubstitution(val keyword: String) : PermutedAlphabet {
-
-        override val name: String = "Simple Substitution with keyword $keyword"
-
-        override val permutedAlphabet: Alphabet = plaintextAlphabet.permuteWithPrefix(keyword.removeDuplicates())
-
-    }
-
-    object Atbash : PermutedAlphabet {
-
-        override val name: String = "Atbash"
-
-        override val permutedAlphabet: Alphabet = plaintextAlphabet.reversed()
-
-    }
-
-    class Autokey(val keyword: String) : Cipher {
-
-        override val name: String = "Autokey with keyword $keyword"
-
-        override fun encipher(plainWords: List<String>): List<String> {
-            // TODO: These gymnastics suggest that the hooks defined in the base interface may not be flexible enough...
-            val plainText: String = plainWords.joinToString(separator = "")
-            val cipherText: String = encipher(
-                plainText = keyword + plainText,
-                index = keyword.length,
-                runningCipherText = ""
-            )
-            return cipherText.splitIntoSameSizeWordsAs(plainWords)
-        }
-
-        override fun decipher(cipherWords: List<String>): List<String> {
-            // TODO: These gymnastics suggest that the hooks defined in the base interface may not be flexible enough...
-            val cipherText: String = cipherWords.joinToString(separator = "")
-            val plainText: String = decipher(
-                cipherText = cipherText.replace(" ", ""),
-                index = 0,
-                runningKeyword = keyword
-            )
-            return plainText.splitIntoSameSizeWordsAs(cipherWords)
-        }
-
-        private fun encipher(plainText: String, index: Int, runningCipherText: String): String =
-            if (index >= plainText.length) {
-                runningCipherText
-            } else {
-                encipher(
-                    plainText = plainText,
-                    index = index + 1,
-                    runningCipherText = runningCipherText + TabulaRecta.encode(
-                        key = plainText[index],
-                        char = plainText[index - keyword.length]
+        fun autoKey(keyword: String): Cipher {
+            fun autoKeyEncipher(
+                keyword: String,
+                plainText: String,
+                index: Int,
+                runningCipherText: String
+            ): String =
+                if (index >= plainText.length) {
+                    runningCipherText
+                } else {
+                    autoKeyEncipher(
+                        keyword = keyword,
+                        plainText = plainText,
+                        index = index + 1,
+                        runningCipherText = runningCipherText + TabulaRecta.encode(
+                            key = plainText[index],
+                            char = plainText[index - keyword.length]
+                        )
                     )
-                )
-            }
+                }
 
-        private fun decipher(cipherText: String, index: Int, runningKeyword: String): String =
-            if (index >= cipherText.length) {
-                runningKeyword.removePrefix(keyword)
-            } else {
-                decipher(
-                    cipherText = cipherText,
-                    index = index + 1,
-                    runningKeyword = runningKeyword + TabulaRecta.decode(
-                        key = runningKeyword[index],
-                        char = cipherText[index]
+            fun autoKeyDecipher(
+                keyword: String,
+                cipherText: String,
+                index: Int,
+                runningKeyword: String
+            ): String =
+                if (index >= cipherText.length) {
+                    runningKeyword.removePrefix(keyword)
+                } else {
+                    autoKeyDecipher(
+                        keyword = keyword,
+                        cipherText = cipherText,
+                        index = index + 1,
+                        runningKeyword = runningKeyword + TabulaRecta.decode(
+                            key = runningKeyword[index],
+                            char = cipherText[index]
+                        )
                     )
-                )
-            }
+                }
+
+            return despacedCipher(
+                name = "Autokey with keyword $keyword",
+                encipher = { despacedPlainText: String ->
+                    autoKeyEncipher(
+                        keyword = keyword,
+                        plainText = keyword + despacedPlainText,
+                        index = keyword.length,
+                        runningCipherText = ""
+                    )
+                },
+                decipher = { despacedCipherText: String ->
+                    autoKeyDecipher(
+                        keyword = keyword,
+                        cipherText = despacedCipherText,
+                        index = 0,
+                        runningKeyword = keyword
+                    )
+                }
+            )
+        }
 
     }
 
 }
+
+// Constructors
+
+fun rawTextCipher(
+    name: String,
+    encipher: (rawPlainText: String) -> String,
+    decipher: (rawCipherText: String) -> String
+): Cipher = object : Cipher {
+
+    override val name: String = name
+
+    override fun encipher(plainText: String): String = encipher(plainText)
+
+    override fun decipher(cipherText: String): String = decipher(cipherText)
+
+}
+
+fun wordedTextCipher(
+    name: String,
+    encipher: (plainWords: List<String>) -> List<String>,
+    decipher: (cipherWords: List<String>) -> List<String>
+): Cipher = rawTextCipher(
+    name,
+    encipher = { rawPlainText: String ->
+        val plainWords: List<String> = rawPlainText.split(' ')
+        val cipherWords: List<String> = encipher(plainWords)
+        cipherWords.joinToString(separator = " ")
+    },
+    decipher = { rawCipherText: String ->
+        val cipherWords: List<String> = rawCipherText.split(' ')
+        val plainWords: List<String> = decipher(cipherWords)
+        plainWords.joinToString(separator = " ")
+    }
+)
+
+fun despacedCipher(
+    name: String,
+    encipher: (despacedPlainText: String) -> String,
+    decipher: (despacedCipherText: String) -> String
+): Cipher = rawTextCipher(
+    name,
+    encipher = { rawPlainText: String ->
+        val despacedCipherText: String = encipher(rawPlainText.replace(" ", ""))
+        despacedCipherText.reInjectSpacesFrom(rawPlainText)
+    },
+    decipher = { rawCipherText: String ->
+        val despacedPlainText: String = decipher(rawCipherText.replace(" ", ""))
+        despacedPlainText.reInjectSpacesFrom(rawCipherText)
+    }
+)
+
+fun substitution(
+    name: String,
+    encipherChar: (plainChar: Char, index: Int) -> Char,
+    decipherChar: (cipherChar: Char, index: Int) -> Char
+): Cipher {
+    fun applySubstitutions(
+        words: List<String>,
+        substitute: (Char, Int) -> Char
+    ): List<String> =
+        words.mapIndexed { wordIndex: Int, word: String ->
+            val numPreviousChars: Int = words.take(wordIndex)
+                .fold(0) { totalChars: Int, previousWord: String ->
+                    totalChars + previousWord.length
+                }
+            word
+                .mapIndexed { charIndex: Int, char: Char ->
+                    val index: Int = numPreviousChars + charIndex
+                    substitute(char, index)
+                }
+                .joinToString(separator = "")
+        }
+
+    return wordedTextCipher(
+        name,
+        encipher = { plainWords: List<String> ->
+            applySubstitutions(plainWords, encipherChar)
+        },
+        decipher = { cipherWords: List<String> ->
+            applySubstitutions(cipherWords, decipherChar)
+        }
+    )
+}
+
+fun permutedAlphabet(
+    name: String,
+    permutedAlphabet: Alphabet
+): Cipher = substitution(
+    name,
+    encipherChar = { plainChar: Char, index: Int ->
+        permutedAlphabet[plaintextAlphabet.indexOf(plainChar)]
+    },
+    decipherChar = { cipherChar: Char, index: Int ->
+        plaintextAlphabet[permutedAlphabet.indexOf(cipherChar)]
+    }
+)
--- src/test/kotlin/CipherTests.kt
+++ src/test/kotlin/CipherTests.kt
@@ -9,59 +9,59 @@ class CipherTests : FreeSpec() {
             forAll(
                 Row3(
                     "MY NAME IS JEFF",
-                    Cipher.Noop,
+                    Cipher.noop,
                     "MY NAME IS JEFF"
                 ),
                 Row3(
                     "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG",
-                    Cipher.Caesar,
+                    Cipher.caesar,
                     "QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD"
                 ),
                 Row3(
                     "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG",
-                    Cipher.Affine(factor = 1, shift = -3),
+                    Cipher.affine(factor = 1, shift = -3),
                     "QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD"
                 ),
                 Row3(
                     "AFFINE",
-                    Cipher.Affine(factor = 5, shift = 8),
+                    Cipher.affine(factor = 5, shift = 8),
                     "IHHWVC"
                 ),
                 Row3(
                     "ATTACKING TONIGHT",
-                    Cipher.Vigenere("OCULORHINOLARINGOLOGY"),
+                    Cipher.vigenere("OCULORHINOLARINGOLOGY"),
                     "OVNLQBPVT HZNZOUZ"
                 ),
                 Row3(
                     "FLEE AT ONCE WE ARE DISCOVERED",
-                    Cipher.SimpleSubstitution("ZEBRAS"),
+                    Cipher.simpleSubstitution("ZEBRAS"),
                     "SIAA ZQ LKBA VA ZOA RFPBLUAOAR"
                 ),
                 Row3(
                     "TUTORIAL",
-                    Cipher.Shift(3),
+                    Cipher.shift(3),
                     "WXWRULDO"
                 ),
                 Row3(
                     "FOO BAR",
-                    Cipher.Atbash,
+                    Cipher.atbash,
                     "ULL YZI"
                 ),
                 Row3(
                     "ATTACK AT DAWN",
-                    Cipher.Autokey("QUEENLY"),
+                    Cipher.autoKey("QUEENLY"),
                     "QNXEPV YT WTWP"
                 ),
                 // Actual solution
                 Row3(
                     "IT IS TRUE HE LIVES AGAIN THROUGH THE GLORY OF THE CHAINED GOD IF YOUR FATHER CANNOT ACCEPT THIS TRUTH THEN YOU WILL MUST TAKE HIS PLACE NEZZNAR SHALL GUIDE THE YUAN TI TO GREATNESS THE END OF THE AGE OF MAN IS NIGH",
-                    Cipher.Vigenere("ECLIPSE"),
+                    Cipher.vigenere("ECLIPSE"),
                     "MV TA IJYI JP TXNIW CRIXF XLTZCVZ XLG RTDJC SH EPT ULEKYMS YSH KQ GDMV JCEPTJ GEPYWI SGGGAB IZMW VCCIZ XLGY GDM AMNW UJKX XCVM WAW TNLKT FIDBYIG KLENW OJAHI VSM NMER VT BD YVICEVTKW XJP MCV SJ VSM PYI SH XIC AW RKRP"
                 ),
                 // Found a bug
                 Row3(
                     "IT IS TRUE HE LIVES AGAIN THROUGH THE GLORY OF THE CHAINED GOD IF YOUR FATHER CANNOT ACCEPT THIS TRUTH THEN YOU WILL MUST TAKE HIS PLACE NEZZNAR SHALL GUIDE THE YUAN TI TO GREATNESS THE END OF THE AGE OF MAN IS NIGH",
-                    Cipher.SimpleSubstitution("ECLIPSE"),
+                    Cipher.simpleSubstitution("ECLIPSE"),
                     "DT DR TQUP BP HDVPR EAEDK TBQMUAB TBP AHMQY MS TBP LBEDKPI AMI DS YMUQ SETBPQ LEKKMT ELLPNT TBDR TQUTB TBPK YMU WDHH JURT TEGP BDR NHELP KPZZKEQ RBEHH AUDIP TBP YUEK TD TM AQPETKPRR TBP PKI MS TBP EAP MS JEK DR KDAB"
                 ),
             ) { plainText: String, cipher: Cipher, cipherText: String ->
@@ -77,51 +77,39 @@ class CipherTests : FreeSpec() {
         }
         "Test cipher properties" - {
             "NOOP" {
-                Cipher.Noop.name shouldBe "NOOP"
+                Cipher.noop.name shouldBe "NOOP"
             }
             "Caesar" {
-                val cipher = Cipher.Caesar
-                cipher.name shouldBe "Caesar"
+                Cipher.caesar.name shouldBe "Affine with formula `1 * x + -3 mod 26`"
             }
             "Affine" {
-                val cipher = Cipher.Affine(factor = 5, shift = 8)
+                val cipher = Cipher.affine(factor = 5, shift = 8)
                 cipher.name shouldBe "Affine with formula `5 * x + 8 mod 26`"
-                cipher.factor shouldBe 5
-                cipher.shift shouldBe 8
-                cipher.inverseFactor shouldBe 21
             }
             "Vigenere" {
-                val cipher = Cipher.Vigenere("OCULORHINOLARINGOLOGY")
+                val cipher = Cipher.vigenere("OCULORHINOLARINGOLOGY")
                 cipher.name shouldBe "Vigenère with keyword OCULORHINOLARINGOLOGY"
-                cipher.keyword shouldBe "OCULORHINOLARINGOLOGY"
             }
             "Simple Substitution" {
-                val cipher = Cipher.SimpleSubstitution("ZEBRAS")
+                val cipher = Cipher.simpleSubstitution("ZEBRAS")
                 cipher.name shouldBe "Simple Substitution with keyword ZEBRAS"
-                cipher.keyword shouldBe "ZEBRAS"
-                cipher.permutedAlphabet shouldBe Alphabet("ZEBRASCDFGHIJKLMNOPQTUVWXY")
             }
             "Simple Substitution when key has duplicate letter" {
                 // I didn't really consider this use case, it probably should throw on initialization but that would
                 // make Main fail, and I'm not going to fix that since the puzzle has been solved with a different cipher
-                val cipher = Cipher.SimpleSubstitution("ECLIPSE")
+                val cipher = Cipher.simpleSubstitution("ECLIPSE")
                 cipher.name shouldBe "Simple Substitution with keyword ECLIPSE"
-                cipher.keyword shouldBe "ECLIPSE"
-                cipher.permutedAlphabet shouldBe Alphabet("ECLIPSABDFGHJKMNOQRTUVWXYZ")
             }
             "Shift" {
-                val cipher = Cipher.Shift(3)
-                cipher.name shouldBe "Shift with shift 3"
-                cipher.shift shouldBe 3
+                val cipher = Cipher.shift(3)
+                cipher.name shouldBe "Affine with formula `1 * x + 3 mod 26`"
             }
             "Atbash" {
-                Cipher.Atbash.name shouldBe "Atbash"
-                Cipher.Atbash.permutedAlphabet shouldBe Alphabet("ZYXWVUTSRQPONMLKJIHGFEDCBA")
+                Cipher.atbash.name shouldBe "Atbash"
             }
             "Autokey" {
-                val cipher = Cipher.Autokey("QUEENLY")
+                val cipher = Cipher.autoKey("QUEENLY")
                 cipher.name shouldBe "Autokey with keyword QUEENLY"
-                cipher.keyword shouldBe "QUEENLY"
             }
         }
     }
